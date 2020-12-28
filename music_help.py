b35a2cbe56863c559039cfa70fb7bac55282c946
Leo Amato <amato979@gmail.com>
2020-12-27 21:49:07 -0300

    Intevals functions

--- music_help.py
+++ music_help.py
@@ -5,3 +5,51 @@ notes = ["C", "C#", "D", "D#", "E", "F",
 intervals = ["Unisono", "2m", "2M", "3m", "3M", "4j", 
 			 "5dim", "5j", "6m", "6M", "7m", "7M", "8j"]
 
+def intervals_calculator (base, interval, sense):
+	index = search_note(base)
+	offset = search_interval (interval)
+	result = 0
+
+	if (offset == 0 or offset == 12):
+		result = index
+
+	if (sense == "asc" and index + offset < 12):
+		result = index + offset
+
+	elif (sense == "asc" and index + offset >= 12):
+		result = abs (12 - (index + offset))
+
+	elif (sense == "dec" and index - offset >= 0):
+		result = index - offset
+
+	else:
+		result = 12 + (index - offset)
+
+	assert (0 <= result and result < 12)
+
+	return notes[result]
+
+def tell_interval (base, dest, sense):
+	base_n = search_note (base)
+	dest_n = search_note (dest)
+	interv = 0
+
+	if (base_n == dest_n):
+		return intervals[12]
+
+	elif (sense == "asc" and base_n < dest_n):
+		interv = (dest_n - base_n)
+
+	elif (sense == "asc" and base_n > dest_n):
+		interv = (12 - base_n) + dest_n
+
+	elif (sense == "dec" and base_n < dest_n):
+		interv = (12 - dest_n) + base_n
+
+	else:
+		interv = (base_n - dest_n);
+
+	assert(0 <= interv and interv <= 12)
+
+	return intervals[interv]
+
