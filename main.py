de72b20dad6ff7ececac3de4b11cb4046d3ee1b9
Leo Amato <amato979@gmail.com>
2020-12-27 23:04:28 -0300

    First aproach of function main

---
+++ main.py
@@ -0,0 +1,39 @@
+# First import the functions
+
+print "********** MUSIC TOOLS **********\n\n"
+
+print "Main menu"
+
+print "1) Calculates intervals"
+print "2) Comprovate interval"
+print "3) Calculates a thriad chord"
+print "4) Comprovate a thriad chord"
+print "5) Calculates a quad chord"
+print "6) Comprovate a quad chord"
+
+
+"""
+# Interval calculator
+
+note = str(raw_input ("Please insert a note \n"))
+
+interval = str(raw_input ("Please insert a interval\n"))
+
+sense = str(raw_input ("Please insert a sense\n"))
+
+result = intervals_calculator (note, interval, sense)
+print(result)
+
+
+# Tell interval
+
+note1 = str(raw_input ("Please insert a note \n"))
+
+note2 = str(raw_input ("Please another note\n"))
+
+sense = str(raw_input ("Please insert a sense\n"))
+
+
+result = tell_interval (note1, note2, sense)
+print(result)
+"""
